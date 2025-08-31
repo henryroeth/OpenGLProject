29648e47e45f5a93d7ae2c8d8af1bbc2707863b5
Henry <henrykroeth@gmail.com>
2025-08-31 18:59:22 -0400

    Updates

--- Assignment1/Assignment1/main.cpp
+++ Assignment1/Assignment1/main.cpp
@@ -1,37 +1,59 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
-int main(void)
+int main()
 {
-    GLFWwindow* window;
+	// Initialize GLFW
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+    // Initialialize window title and size
+	int width = 800;
+	int height = 600;
+    char title[] = "Assignment 1";
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(800, 600, "Assignment 1", NULL, NULL);
-    if (!window)
+	// Create a windowed mode window and its OpenGL context
+    GLFWwindow* window = glfwCreateWindow(width, height, title, NULL, NULL);
+    if (window == NULL)
     {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
-
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
+	// Load OpenGL function pointers with GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+	// Set the viewport
+    glViewport(0, 0, width, height);
+
+	// Register a callback function to adjust the viewport when the window is resized
+    void framebuffer_size_callback(GLFWwindow * window, int width, int height);
+
+	// Register the callback function
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Render loop
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
         glfwSwapBuffers(window);
-
-        /* Poll for and process events */
         glfwPollEvents();
     }
 
+	// Clean up and exit
     glfwTerminate();
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
 }
